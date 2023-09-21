dbd54fb7faae201fcd886aa49e60e12b6d51962c
Sæmundur Ásgrímsson <saemiolf@gmail.com>
2023-09-21 09:47:05 +0000

    .

--- Hopverkefni.py
+++ Hopverkefni.py
@@ -8,7 +8,10 @@ def checkbox(x,y):
     elif x == 1 and y == 2:
         movement = "N S W"
     elif x == 1 and y == 3:
-        movement = "S W"
+        movement = "S A"
+    #elif x == 1 and y ==4:
+        #movement = "N S"
+
 
 x = 1
 y = 1
@@ -20,5 +23,54 @@ while OnOff:
         choice = input()
         if choice == "N":
             y += 1
+        else:
+            print("Direction: Not a valid direction!")
+
+    elif x == 1 and y == 2:
+        print("You can  travel (N)orth, (S)outh, (E(ast. " )
+        choice = input()
+        if choice == "N":
+            y += 1
+        elif choice == "S":
+            y -= 1    
+        elif choice == "E":
+            x += 1
+        else:
+            print("Direction: Not a valid direction!")
+    
+    elif x == 1 and y == 3:
+        print("You can travel: (S)outh, and (E)ast. " )
+        choice = input()
+        if choice == "S":
+            y -= 1
+        elif choice == "E":
+            x += 1
+        else:
+            print("Direction: Not a valid direction!")
+
+    elif x == 2 and y == 1:
+        print("You can travel: (N)" )
+        choice = input()
+        if choice == "N":
+            x += 1
+        else:
+            print("Direction: Not a valid direction!")
+
+    if x == 2 and y == 2:
+        print("You can travel: (S)outh, and (W)est. ")
+        choice = input()
+        if choice == "S":
+            y -= 1
+        elif choice == "W":
+            x -= 1
+        else:
+            print("Direction: Not a valid direction!")
+    if x == 2 and y == 3:
+        print("You can travel: (E)ast, and (W)est. ")
+        choice = input()
+        if choice == "E":
+            x += 1
+        elif choice == "W":
+            x -= 1
         else:
             print("Direction: Not a valid direction!")
